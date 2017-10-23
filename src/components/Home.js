a1070220ab2e0c487f83651d5f90413b6d269827
Jae Easley <jaesley@gmail.com>
2017-10-23 16:54:16 -0700

    Restructure folders

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
+import profile_photo from '../images/profile_photo.jpeg'
 
 class Home extends Component {
   render() {
     return (
       <div className='Home'>
-
-      <div className='image'>
-        <img src='../public/profile_photo.jpg' />
-      </div>
+        <img src={profile_photo} className='image' />
 
         When I ran bookstores, I always imagined myself living in a book. I just didn't realize that book would be Cracking the Coding Interview. My life as a web developer started with mastering Excel and Mediawiki for enhancing all my tabletop games; now I spend my time building apps to find me more books and lobbying Spotify to bring back artist location info in their API. I still love pivot tables. 
 
